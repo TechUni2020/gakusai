8b7f1a58f3ac53e4fab4a592f0213fdf216a619b
code-yy <jiyeyouren523@gmail.com>
2022-10-04 23:59:16 +0900

    🎉 プロジェクトの作成

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import { Input } from "@mantine/core";
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return <Input size="lg" color="red" />;
+};
+
+export default Home;
