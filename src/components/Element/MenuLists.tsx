4e843e16b257c0aa7aca9e847c35e108a4c07041
kawabata <s1931115@s.konan-u.ac.jp>
2022-10-26 15:19:47 +0900

    :sparkles: DRY MenulistをComponent化

--- src/components/Element/MenuLists.tsx
+++ src/components/Element/MenuLists.tsx
@@ -1,8 +1,7 @@
-import { SimpleGrid, Tabs } from "@mantine/core";
+import { Tabs } from "@mantine/core";
 import { FC } from "react";
-import { Menu } from "@/type/Menu";
-import { MenuCard } from "@/components/Element/MenuCard";
 import { useFetchMenuList } from "@/hooks/useFetchMenuList";
+import { BaseMenuList } from "@/components/Element/BaseMenuList";
 
 export const MenuLists: FC = () => {
   const { menuList } = useFetchMenuList();
@@ -20,51 +19,15 @@ export const MenuLists: FC = () => {
       </Tabs.List>
 
       <Tabs.Panel value="all" mt={4}>
-        <SimpleGrid
-          cols={4}
-          mx={24}
-          breakpoints={[
-            { maxWidth: "md", cols: 4, spacing: "md" },
-            { maxWidth: "sm", cols: 3, spacing: "sm" },
-            { maxWidth: "xs", cols: 2, spacing: "sm" },
-          ]}
-        >
-          {filteredAllMenuList.map((menu: Menu) => (
-            <MenuCard key={menu.id} menu={menu} />
-          ))}
-        </SimpleGrid>
+        <BaseMenuList menuList={filteredAllMenuList} />
       </Tabs.Panel>
 
       <Tabs.Panel value="food" mt={4}>
-        <SimpleGrid
-          cols={4}
-          mx={24}
-          breakpoints={[
-            { maxWidth: "md", cols: 4, spacing: "md" },
-            { maxWidth: "sm", cols: 3, spacing: "sm" },
-            { maxWidth: "xs", cols: 2, spacing: "sm" },
-          ]}
-        >
-          {filteredFoodMenuList.map((menu: Menu) => (
-            <MenuCard key={menu.id} menu={menu} />
-          ))}
-        </SimpleGrid>
+        <BaseMenuList menuList={filteredFoodMenuList} />
       </Tabs.Panel>
 
       <Tabs.Panel value="drink" mt={4}>
-        <SimpleGrid
-          cols={4}
-          mx={24}
-          breakpoints={[
-            { maxWidth: "md", cols: 4, spacing: "md" },
-            { maxWidth: "sm", cols: 3, spacing: "sm" },
-            { maxWidth: "xs", cols: 2, spacing: "sm" },
-          ]}
-        >
-          {filteredDrinkMenuList.map((menu: Menu) => (
-            <MenuCard key={menu.id} menu={menu} />
-          ))}
-        </SimpleGrid>
+        <BaseMenuList menuList={filteredDrinkMenuList} />
       </Tabs.Panel>
     </Tabs>
   );
