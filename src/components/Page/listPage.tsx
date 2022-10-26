8b29223537c10176fdae6413a0b317fd388a7c41
kawabata <s1931115@s.konan-u.ac.jp>
2022-10-26 15:47:07 +0900

    :twisted_rightwards_arrows: conflictの解消とmerge

--- src/components/Page/listPage.tsx
+++ src/components/Page/listPage.tsx
@@ -1,10 +1,12 @@
 import { Box, Center } from "@mantine/core";
 import { FC } from "react";
 import { MenuLists } from "@/components/Element/MenuLists";
+import { AuthModal } from "../authModal";
 
 export const ListPage: FC = () => {
   return (
     <>
+      <AuthModal />
       <Box mt={24} mx={24}>
         <Center>以下から商品を注文できます。</Center>
         <Center>※お支払いは、、、。</Center>
