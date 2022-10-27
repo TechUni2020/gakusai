aa4d7d642250b56f495c165f46cac5aed6db9f64
ponsAoki <aokishogo20pk@gmail.com>
2022-10-28 02:38:28 +0900

    商品購入したい

--- src/components/Element/header.tsx
+++ src/components/Element/header.tsx
@@ -1,14 +1,22 @@
-import { Center, Divider, Title } from "@mantine/core";
+import { Center, Divider, Grid, Title } from "@mantine/core";
 import { FC } from "react";
+import { CartToggle } from "../cartToggle";
 
 export const Header: FC = () => {
   return (
     <>
-      <Center>
-        <Title order={3} my={8} color="orange">
-          Mobile Order
-        </Title>
-      </Center>
+      <Grid>
+        <Grid.Col span="content">
+          <CartToggle />
+        </Grid.Col>
+        <Grid.Col span="content">
+          <Center>
+            <Title order={3} my={8} color="orange">
+              Mobile Order
+            </Title>
+          </Center>
+        </Grid.Col>
+      </Grid>
       <Divider color="orange"></Divider>
     </>
   );
