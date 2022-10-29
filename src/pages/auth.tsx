c4566fd182e8eb9b61afe2b47f3d49e3058d3edf
ユウト <codeyy021@gmail.com>
2022-10-29 22:12:48 +0900

    🔒️ ログインしてる場合、ログインしていない場合にページを見れないように

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,7 +1,21 @@
 import { NextPage } from "next/types";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import { Auth } from "@/components/Page/auth";
+import { AppLoading } from "@/components/Element/AppLoading";
+import { pagesPath } from "@/lib/$path";
+import { authService } from "@/modules/auth/auth.service";
 
 const AuthPage: NextPage = () => {
+  const router = useRouter();
+  const isSignedIn = authService.isSignedIn();
+
+  useEffect(() => {
+    if (isSignedIn) router.push(pagesPath.$url());
+  }, []);
+
+  if (isSignedIn) return <AppLoading />;
+
   return <Auth />;
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,24 @@
+import { Provider } from "react-redux";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 import { PageLayout } from "@/components/Layout/pageLayout";
 import { ListPage } from "@/components/Page";
-import type { NextPage } from "next";
-import { Provider } from "react-redux";
+import { pagesPath } from "@/lib/$path";
+import { authService } from "@/modules/auth/auth.service";
+import { AppLoading } from "@/components/Element/AppLoading";
 import store from "../store";
+import type { NextPage } from "next";
 
 const Home: NextPage = () => {
+  const router = useRouter();
+  const isSignedIn = authService.isSignedIn();
+
+  useEffect(() => {
+    if (!isSignedIn) router.push(pagesPath.auth.$url());
+  }, []);
+
+  if (!isSignedIn) return <AppLoading />;
+
   return (
     <Provider store={store}>
       <PageLayout>
