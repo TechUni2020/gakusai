48bf291508c8e1825dfa47ece6174c545c650e72
kawabata <s1931115@s.konan-u.ac.jp>
2022-11-03 10:07:43 +0900

    :sparkles: order番号の画面で必要な情報を表示

--- src/components/Page/completed.tsx
+++ src/components/Page/completed.tsx
@@ -1,23 +1,25 @@
-import { FC, useEffect, useState } from "react";
-import { Box, Button, Card, createStyles, Modal } from "@mantine/core";
+import { FC, useState } from "react";
+import { Text, Box, Button, Card, createStyles, Modal } from "@mantine/core";
 import { useRouter } from "next/router";
 import { useSetRecoilState } from "recoil";
-import { orderService } from "@/modules/order/order.service";
 import { TOKEN_LABEL } from "@/constants/token_label";
 import { pagesPath } from "@/lib/$path";
 import { CartState } from "@/globalStates/atoms/cartState";
+import { useOrderState } from "@/globalStates/atoms/orderState";
+import { OrderDetailList } from "@/components/OrderDetailList";
 
 export const Completed: FC = () => {
   const { classes } = useStyles();
-  const [orderId, setOrderId] = useState<string | null>(null);
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const { ORDER_ID } = TOKEN_LABEL;
   const router = useRouter();
   const setCart = useSetRecoilState(CartState);
+  const { order } = useOrderState();
 
-  useEffect(() => {
-    setOrderId(orderService.getOrderNum());
-  }, []);
+  if (!order) {
+    return null;
+  }
+  const { orderId, orderedAt, detail, sumPrice } = order;
 
   const onClickCheckButton = () => {
     setIsOpen(true);
@@ -26,20 +28,29 @@ export const Completed: FC = () => {
     setCart([]);
   };
 
-  if (orderId == null) return null;
   return (
     <Card className={classes.card}>
       <Box>ご注文ありがとうございます。</Box>
-      <Box className={classes.orderNumberWrapper}>
+      <Box className={classes.orderColumnWrapper}>
         <Box>お客様番号</Box>
         <Box className={classes.orderNumber}>{`『${orderId}』`}</Box>
+      </Box>
+      <Box className={classes.orderColumnWrapper}>
+        <Box>注文時間</Box>
+        <Text>{String(orderedAt)}</Text>
+      </Box>
+      <OrderDetailList list={detail} />
+      <Box className={classes.orderColumnWrapper}>
+        <Box>合計金額</Box>
+        <Text size="xl">{sumPrice}円</Text>
       </Box>
 
       <Box>この画面をスクショしてスタッフに見せてください！</Box>
 
       <Button onClick={() => setIsOpen(true)}>商品を受け取りました</Button>
       <Modal opened={isOpen} onClose={() => setIsOpen(false)}>
-        <Box>この注文画面には戻れなくなりますがよろしいでしょうか？？</Box>
+        <Text size="sm">この注文画面には戻れません</Text>
+        <Text size="sm">よろしいですか?</Text>
         <Button onClick={onClickCheckButton}>確認</Button>
       </Modal>
     </Card>
@@ -53,12 +64,13 @@ const useStyles = createStyles({
     backgroundColor: "orange",
     borderRadius: "16px",
     margin: 16,
+    height: "100%",
   },
-  orderNumberWrapper: {
+  orderColumnWrapper: {
     display: "flex",
     alignItems: "center",
     gap: 5,
-    margin: "10px 0",
+    marginTop: "10px",
   },
   orderNumber: {
     fontSize: "30px",
