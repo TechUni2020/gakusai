e2ab4afa27ef17c4e40541bea2ace2af619095e1
ponsAoki <aokishogo20pk@gmail.com>
2022-11-03 22:26:42 +0900

    orderDetailListのmenuId変更漏れ

--- src/components/OrderDetailList.tsx
+++ src/components/OrderDetailList.tsx
@@ -1,7 +1,7 @@
+import { CartState } from "@/globalStates/atoms/cartState";
+import { Box, List, Text } from "@mantine/core";
 import { FC } from "react";
 import { useRecoilValue } from "recoil";
-import { Box, List, Text } from "@mantine/core";
-import { CartState } from "@/globalStates/atoms/cartState";
 
 export const OrderDetailList: FC = () => {
   const cart = useRecoilValue(CartState);
@@ -15,7 +15,7 @@ export const OrderDetailList: FC = () => {
         注文内容一覧
       </Text>
       {cart.map((item) => (
-        <List key={item.menuId} mt={5} withPadding>
+        <List key={item.menu_id} mt={5} withPadding>
           <List.Item>
             <Text size="md">
               {item.name} ︎ {item.quantity}
