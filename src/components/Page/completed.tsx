570d93f28faceae7cc8fc144cfa18def37151f64
kawabata <s1931115@s.konan-u.ac.jp>
2022-11-03 12:46:29 +0900

    :construction: 作業途中

--- src/components/Page/completed.tsx
+++ src/components/Page/completed.tsx
@@ -1,78 +1,79 @@
-import { FC, useState } from "react";
-import { Text, Box, Button, Card, createStyles, Modal } from "@mantine/core";
-import { useRouter } from "next/router";
-import { useSetRecoilState } from "recoil";
-import { TOKEN_LABEL } from "@/constants/token_label";
-import { pagesPath } from "@/lib/$path";
-import { CartState } from "@/globalStates/atoms/cartState";
-import { useOrderState } from "@/globalStates/atoms/orderState";
-import { OrderDetailList } from "@/components/OrderDetailList";
+import {FC, useEffect, useState} from "react";
+import {Text, Box, Button, Card, createStyles, Modal} from "@mantine/core";
+import {useRouter} from "next/router";
+import {useSetRecoilState} from "recoil";
+import {CartState} from "@/globalStates/atoms/cartState";
+import {useOrderState} from "@/globalStates/atoms/orderState";
+import {OrderDetailList} from "@/components/OrderDetailList";
+import {collection, query, where, onSnapshot} from "firebase/firestore";
+import {db} from "@/lib/firebase";
 
 export const Completed: FC = () => {
-  const { classes } = useStyles();
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-  const { ORDER_ID } = TOKEN_LABEL;
-  const router = useRouter();
-  const setCart = useSetRecoilState(CartState);
-  const { order } = useOrderState();
+    const {classes} = useStyles();
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+    // const router = useRouter();
+    // const setCart = useSetRecoilState(CartState);
+    const {order} = useOrderState();
 
-  if (!order) {
-    return null;
-  }
-  const { orderId, orderedAt, detail, sumPrice } = order;
+    if (!order) {
 
-  const onClickCheckButton = () => {
-    setIsOpen(true);
-    localStorage.removeItem(ORDER_ID);
-    router.push(pagesPath.$url());
-    setCart([]);
-  };
+        return null;
+    }
+    const {orderId, orderedAt, detail, sumPrice} = order;
 
-  return (
-    <Card className={classes.card}>
-      <Box>ご注文ありがとうございます。</Box>
-      <Box className={classes.orderColumnWrapper}>
-        <Box>お客様番号</Box>
-        <Box className={classes.orderNumber}>{`『${orderId}』`}</Box>
-      </Box>
-      <Box className={classes.orderColumnWrapper}>
-        <Box>注文時間</Box>
-        <Text>{String(orderedAt)}</Text>
-      </Box>
-      <OrderDetailList list={detail} />
-      <Box className={classes.orderColumnWrapper}>
-        <Box>合計金額</Box>
-        <Text size="xl">{sumPrice}円</Text>
-      </Box>
+    // const onClickCheckButton = () => {
+    //   setIsOpen(true);
+    //   localStorage.removeItem(ORDER_ID);
+    //   router.push(pagesPath.$url());
+    //   setCart([]);
+    // };
 
-      <Box>この画面をスクショしてスタッフに見せてください！</Box>
+    return (
+        <Card className={classes.card}>
+            <Box>ご注文ありがとうございます。</Box>
+            <Box className={classes.orderColumnWrapper}>
+                <Box>お客様番号</Box>
+                <Box className={classes.orderNumber}>{`『${orderId}』`}</Box>
+            </Box>
+            <Box className={classes.orderColumnWrapper}>
+                <Box>注文時間</Box>
+                <Text>{String(orderedAt)}</Text>
+            </Box>
+            <OrderDetailList list={detail}/>
+            <Box className={classes.orderColumnWrapper}>
+                <Box>合計金額</Box>
+                <Text size="xl">{sumPrice}円</Text>
+            </Box>
 
-      <Button onClick={() => setIsOpen(true)}>商品を受け取りました</Button>
-      <Modal opened={isOpen} onClose={() => setIsOpen(false)}>
-        <Text size="sm">この注文画面には戻れません</Text>
-        <Text size="sm">よろしいですか?</Text>
-        <Button onClick={onClickCheckButton}>確認</Button>
-      </Modal>
-    </Card>
-  );
+            <Box>この画面をスクショしてスタッフに見せてください！</Box>
+
+            {/* Todo このボタンを自動化したい */}
+            {/*<Button onClick={() => setIsOpen(true)}>商品を受け取りました</Button>*/}
+            {/*<Modal opened={isOpen} onClose={() => setIsOpen(false)}>*/}
+            {/*  <Text size="sm">この注文画面には戻れません</Text>*/}
+            {/*  <Text size="sm">よろしいですか?</Text>*/}
+            {/*  <Button >確認</Button>*/}
+            {/*</Modal>*/}
+        </Card>
+    );
 };
 
 const useStyles = createStyles({
-  card: {
-    color: "white",
-    fontWeight: "bold",
-    backgroundColor: "orange",
-    borderRadius: "16px",
-    margin: 16,
-    height: "100%",
-  },
-  orderColumnWrapper: {
-    display: "flex",
-    alignItems: "center",
-    gap: 5,
-    marginTop: "10px",
-  },
-  orderNumber: {
-    fontSize: "30px",
-  },
+    card: {
+        color: "white",
+        fontWeight: "bold",
+        backgroundColor: "orange",
+        borderRadius: "16px",
+        margin: 16,
+        height: "100%",
+    },
+    orderColumnWrapper: {
+        display: "flex",
+        alignItems: "center",
+        gap: 5,
+        marginTop: "10px",
+    },
+    orderNumber: {
+        fontSize: "30px",
+    },
 });
--- src/globalStates/atoms/orderState/index.ts
+++ src/globalStates/atoms/orderState/index.ts
@@ -1,21 +1,23 @@
-import { useEffect, useState } from "react";
-import { atom, RecoilState, useRecoilState } from "recoil";
-import { recoilPersist } from "recoil-persist";
-import { Order } from "@/type/Order";
-import { TOKEN_LABEL } from "@/constants/token_label";
+import {useEffect, useState} from "react";
+import {atom, RecoilState, useRecoilState} from "recoil";
+import {recoilPersist} from "recoil-persist";
+import {Order} from "@/type/Order";
+import {TOKEN_LABEL} from "@/constants/token_label";
+import {collection, onSnapshot, query, where} from "firebase/firestore";
+import {db} from "@/lib/firebase";
 
-const { ORDER_STATE } = TOKEN_LABEL;
+const {ORDER_STATE} = TOKEN_LABEL;
 
-const { persistAtom } = recoilPersist({
-  key: ORDER_STATE,
+const {persistAtom} = recoilPersist({
+    key: ORDER_STATE,
 });
 
 export const initialState = {};
 
 export const OrderState: RecoilState<Order> = atom({
-  key: "Order",
-  default: initialState,
-  effects_UNSTABLE: [persistAtom],
+    key: "Order",
+    default: initialState,
+    effects_UNSTABLE: [persistAtom],
 });
 
 /*
@@ -27,16 +29,27 @@ export const OrderState: RecoilState<Order> = atom({
  */
 
 export const useOrderState = () => {
-  const [isInitial, setIsInitial] = useState(true);
-  const [orderState, setOrderState] = useRecoilState(OrderState);
+    const [isInitial, setIsInitial] = useState(true);
+    const [orderState, setOrderState] = useRecoilState(OrderState);
 
-  // 初期レンダリング時にflagをfalseにする
-  useEffect(() => {
-    setIsInitial(false);
-  }, []);
+    // Todo 対象のOrderのみをSubscribeする
+    // console.log(orderState.orderId)
+    // const orderId = orderState.orderId
+    // const q = query(collection(db, "order"))
+    // const unsubscribe = onSnapshot(q, (querySnapshot) => {
+    //     querySnapshot.forEach((doc) => {
+    //         console.log(doc.data())
+    //     });
+    // })
 
-  return {
-    order: isInitial ? null : orderState,
-    setOrder: setOrderState,
-  };
+    // 初期レンダリング時にflagをfalseにする
+    useEffect(() => {
+        setIsInitial(false);
+        // unsubscribe()
+    }, []);
+
+    return {
+        order: isInitial ? null : orderState,
+        setOrder: setOrderState,
+    };
 };
