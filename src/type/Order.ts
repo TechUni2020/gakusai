81aae2e57cde79137a2609aecd2ffdb39682bef1
kawabata <s1931115@s.konan-u.ac.jp>
2022-11-03 10:04:26 +0900

    :sparkles: Orderをrecoilに乗せた

--- src/type/Order.ts
+++ src/type/Order.ts
@@ -1,1 +1,14 @@
+import { Cart } from "./Cart";
+
 export type ReceivingTimeKey = "now" | "5min" | "10min" | "20min" | "30min" | "60min";
+
+/* userUidはセキュリティ上保持しない */
+export type Order = {
+  orderId: string;
+  detail: Cart;
+  isReceived: false;
+  orderedAt: Date;
+  receivingPredictionTime: Date;
+  sumPrice: number;
+  sumQuantity: number;
+};
