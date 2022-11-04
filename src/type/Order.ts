25730a0969d6cdb7ce920dc5454e5ab8b1c3f889
kawabata <s1931115@s.konan-u.ac.jp>
2022-11-04 14:05:58 +0900

    :bug: 型が間違っていたので修正

--- src/type/Order.ts
+++ src/type/Order.ts
@@ -1,3 +1,4 @@
+import { Timestamp } from "@firebase/firestore";
 import { Cart } from "./Cart";
 
 export type ReceivingTimeKey = "now" | "5min" | "10min" | "20min" | "30min" | "60min";
@@ -7,8 +8,8 @@ export type Order = {
   orderId: string;
   detail: Cart;
   isReceived: false;
-  orderedAt: Date;
-  receivingPredictionTime: Date;
+  orderedAt: Timestamp;
+  receivingPredictionTime: Timestamp;
   sumPrice: number;
   sumQuantity: number;
 };
